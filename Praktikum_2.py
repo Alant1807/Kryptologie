d4e485c986a8dca83a9bb6cd2225d24fa12ee357
Alan <alantofeq@hotmail.de>
2022-11-10 13:19:41 +0100

    initial commit

--- Praktikum_2.py
+++ Praktikum_2.py
@@ -7,7 +7,7 @@ def miller_rabin_test(n, it):
         tmp = pow(it, k, n)
         if tmp != 1 and tmp != (n - 1):
             return 1
-        elif tmp == (n - 1) or k % 2 != 0:
+        elif tmp == (n - 1) or (k % 2 != 0 and tmp == 1 or tmp == (n - 1)):
             return 0
         k //= 2
 
@@ -31,7 +31,7 @@ def anzahl_zeugen(n):
 def avg_distance_Prim(anz, n, it):
     summe_differenz = 0
     for counter in range(anz + 1):
-        m = random.randint(0, n)
+        m = random.randint(1, n - 1)
         pm = find_Primzahl(m, it)
         differenz = pm - m
         summe_differenz += differenz
@@ -39,4 +39,4 @@ def avg_distance_Prim(anz, n, it):
 
 
 if __name__ == "__main__":
-    print(avg_distance_Prim(333,100000,100))
+    print(avg_distance_Prim(1000, 1))
